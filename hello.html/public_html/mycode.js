e5eb49c3a50ee13d92cb0e97fe035c11c622814e
Andrew Reyes <acreyes0704@gmail.com>
2019-01-03 01:33:07 -0600

    Lesson1 & Lesson2 and some notes

---
+++ hello.html/public_html/mycode.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var productName = "Leather Wallet";
+
+alert(productName);
+
+alert(document.lastModified);
+
