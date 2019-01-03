471c01dc6daf83a8b1cb9f51a3ecda0fa66f50d8
Andrew Reyes <acreyes0704@gmail.com>
2019-01-03 15:57:51 -0600

    A simple Celsius To Fahrenheit converter.

---
+++ Converter/public_html/Converter.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var cTemp = 25;
+var hTemp = ((cTemp * 9) /5) + 32;
+
+document.write("Temperature in Celsius: " + cTemp + " degrees <br/>");
+document.write("Temperature in Fahrenheit: " + hTemp + " degrees");
+
